8309068db12ad2ba9b40b05eb9c30bd40b53333d
Utkarsh Yadav <72760223+iamutkarshyadav@users.noreply.github.com>
2023-02-14 21:43:27 +0530

    updated the algorithm of the code

--- Question2.cpp
+++ Question2.cpp
@@ -40,3 +40,33 @@ int main() {
 
     return 0;
 }
+ // algorithm of the code
+//Initialize an array of positive integers, an integer variable 'n' to store the size of the array, and an integer variable 'key' to store the value of the key element to search for.
+
+// Initialize an integer variable 'comparisons' to 0, which will be used to count the number of comparisons made during the search.
+
+// Call the binarySearch function, passing in the array, the first index (l) and the last index (r), the 'key' element to search for, and the 'comparisons' variable.
+
+// The binarySearch function implements a recursive algorithm for binary search:
+
+// Check if the last index (r) is greater than or equal to the first index (l). If not, return -1 to indicate that the 'key' element was not found.
+
+// Calculate the middle index of the array, and compare the value at that index to the 'key' element.
+
+// If the value at the middle index is equal to the 'key' element, return the index.
+
+// If the value at the middle index is greater than the 'key' element, call the binarySearch function recursively with the left half of the array, from the first index to the middle index - 1.
+
+// If the value at the middle index is less than the 'key' element, call the binarySearch function recursively with the right half of the array, from the middle index + 1 to the last index.
+
+// The recursion continues until the 'key' element is found or the left and right indices converge.
+
+// After the binary search completes, output the results based on the value of the returned index.
+
+// If the returned index is -1, output a message indicating that the 'key' element was not found in the array.
+
+// If the returned index is not -1, output a message indicating the index at which the 'key' element was found.
+
+// Also output the total number of comparisons made during the search.
+
+// The binary search algorithm used in this code has a time complexity of O(log n), where n is the size of the array
